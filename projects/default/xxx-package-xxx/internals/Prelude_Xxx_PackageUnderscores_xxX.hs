786a5e3bba8f4e31bd739578d1317453570c3e1c
Spiros Boosalis <SamBoosalis@gmail.com>
2018-09-23 17:40:18 -0700

    [default] rename files/dirs

---
+++ projects/default/xxx-package-xxx/internals/Prelude_Xxx_PackageUnderscores_xxX.hs
@@ -0,0 +1,17 @@
+--------------------------------------------------
+
+{- |
+
+a project-specific custom prelude.
+
+-}
+
+module Prelude_Xxx_PackageUnderscores_xxX
+    ( module X
+    ) where
+
+--------------------------------------------------
+
+import "spiros" Prelude.Spiros as X
+
+--------------------------------------------------
