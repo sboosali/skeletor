562e728a0cc57be42bcfc0d9d32bfd7bec2d5741
Sam Boosalis <SamBoosalis@gmail.com>
2018-12-25 21:58:14 -0800

    add « FileBy »

--- docs/NOTES.md
+++ docs/NOTES.md
@@ -15,6 +15,10 @@
 
 --
 
+instance (NFData k, NFData v) => NFData (HashMap k v)
+
+--
+
 readFile :: FilePath -> IO ByteString
 
 writeFile :: FilePath -> ByteString -> IO ()
@@ -28,6 +32,24 @@ The readFile function reads a file and returns the contents of the file as a str
 writeFile :: FilePath -> Text -> IO ()
 
 Write a string to a file. The file is truncated to zero length before writing begins.
+
+--
+
+  class Hashable a where
+    hashWithSalt :: Int -> a -> Int 
+    infixl 0
+
+The class of types that can be converted to a hash value.
+
+Minimal implementation: hashWithSalt.
+
+Return a hash value for the argument, using the given salt.
+
+--
+
+18,446,744,073,709,551,615 ≡ 2^64 − 1
+
+--
 
 --
 
--- skeletor/library/Skeletor/Haskell/Types.hs
+++ skeletor/library/Skeletor/Haskell/Types.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BinaryLiterals #-}
 
 --------------------------------------------------
 --------------------------------------------------
@@ -11,6 +12,14 @@
 module Skeletor.Haskell.Types where
 
 --------------------------------------------------
+--------------------------------------------------
+
+--------------------------------------------------
+--------------------------------------------------
+
+-- import           "modern-uri" Text.URI (URI)
+-- import qualified "modern-uri" Text.URI as URI
+
 --------------------------------------------------
 
 import qualified "unordered-containers" Data.HashMap.Lazy as HashMap
@@ -29,7 +38,7 @@ import Prelude_skeletor
 --------------------------------------------------
 --------------------------------------------------
 
-type UnknownOr = Either String    -- TODO
+type UnknownOr = Either String    -- TODO mv to own pkg.
 
 --------------------------------------------------
 --------------------------------------------------
@@ -74,20 +83,43 @@ defaultKnownProject = DefaultHaskellProject
 
 -}
 
+type Fiels_ = Files ()
+
+--------------------------------------------------
+
+{-|
+
+-}
+
+type FileTemplates = Files TemplateFile
+
+--------------------------------------------------
+
+{-|
+
+-}
+
+type FilesBy = Files FileBy
+
+--------------------------------------------------
+--------------------------------------------------
+
+{-|
+
+-}
+
 newtype Files a = Files
 
   (HashMap FilePath a)
 
   deriving stock    (Functor,Foldable,Traversable)
-  deriving stock    (Show,Read,Generic)
-  deriving newtype  (Eq,Ord,Semigroup,Monoid)
-  deriving newtype  (NFData)
+  deriving stock    (Show,Read)
+  deriving stock    (Generic,Generic1)
+  deriving newtype  (Eq,Ord)
+--deriving newtype  (Semigroup,Monoid) --TODO
+
+  deriving newtype  (NFData,Hashable)
   deriving newtype  (Hashable1)
-
---------------------------------------------------
-
--- | 
-deriving anyclass instance (Hashable a) => Hashable (Files a)
 
 --------------------------------------------------
 
@@ -97,6 +129,16 @@ instance (Ord a) => IsList (Files a) where
   type Item (Files a) = (FilePath, a)
   fromList = HashMap.fromList > coerce
   toList   = coerce           > HashMap.toList
+
+--------------------------------------------------
+
+-- instance NFData1 Files where
+
+--   liftRnf :: (a -> ()) -> (Files a -> ())
+--   liftRnf rnf' = go
+
+--     where
+--     go _ = _
 
 --------------------------------------------------
 
@@ -110,6 +152,55 @@ instance (Ord a) => IsList (Files a) where
 
 emptyFiles :: Files a
 emptyFiles = Files HashMap.empty
+
+--------------------------------------------------
+--------------------------------------------------
+
+{-|
+
+-}
+
+data FileBy
+
+  = FileByContents Text         -- ^ Write this string.
+  | FileByPath     FilePath     -- ^ Copy this file.
+--TODO | FileByUrl      URI          -- ^ Download this URL, then write it.
+
+  deriving stock    (Show,Eq,Ord,Generic)
+  deriving anyclass (NFData)
+
+--------------------------------------------------
+
+instance Hashable FileBy where
+
+  hashWithSalt :: Int -> FileBy -> Int
+  hashWithSalt salt = \case
+
+    FileByContents text -> c1 `hashWithSalt` salt `hashWithSalt` text
+    FileByPath     path -> c2 `hashWithSalt` salt `hashWithSalt` path
+--TODO    FileByUrl      url  -> c3 `hashWithSalt` salt `hashWithSalt` url
+
+    where
+
+    c1 :: Int
+    c1 = 0b0000000000000000000000000000000000000000000000000000000000000000
+
+    c2 :: Int
+    c2 = 0b0101010101010101010101010101010101010101010101010101010101010101
+
+--TODO c3 = :: Int
+--    c3 = 0b1010101010101010101010101010101010101010101010101010101010101010
+
+--------------------------------------------------
+--------------------------------------------------
+
+{-|
+
+-}
+
+data TemplateFile = TemplateFile
+  {
+  }
 
 --------------------------------------------------
 --------------------------------------------------
