433dc6607bf2b9f6c7cbcf001097a939c29b4913
Sam Boosalis <SamBoosalis@gmail.com>
2018-12-27 05:25:39 -0800

    add « ChiaroscuroFilters », « Semigroup/Monoid » instances

--- skeletor/library/Skeletor/Haskell/Types.hs
+++ skeletor/library/Skeletor/Haskell/Types.hs
@@ -38,6 +38,10 @@ import qualified "containers" Data.Set as Set
 import           "containers" Data.Set (Set)
 
 --------------------------------------------------
+
+import qualified "base" Data.Foldable as Fold
+
+--------------------------------------------------
 --------------------------------------------------
 
 import Prelude_skeletor
@@ -250,74 +254,277 @@ emptyFileTree = FileTree Map.empty
 --------------------------------------------------
 --------------------------------------------------
 
-{-| 
+{-|
+
+'ChiaroscuroFilters' is a pair of filters (a blacklist and a whitelist)
+of 'GlobPattern's.
+
+@≡ 'These' 'emptyBlacklist' 'fullWhitelist'@
 
 -}
 
-type FilePathRegex = GlobPattern
+data ChiaroscuroFilters = ChiaroscuroFilters
+
+  { chiaroscuroBlacklist :: !Blacklist
+  , chiaroscuroWhitelist :: !Whitelist
+  }
+
+  deriving stock    (Generic)
+  deriving stock    (Show, Read)
+  deriving stock    (Eq, Ord)
+
+  deriving anyclass (NFData{-, Hashable-})
 
 --------------------------------------------------
+
+-- | @≡ 'noChiaroscuroFilters'@
+
+instance Default ChiaroscuroFilters where
+
+  def = noChiaroscuroFilters
+
 --------------------------------------------------
 
-{-| 
+-- | @≡ 'noChiaroscuroFilters'@
+
+instance Monoid ChiaroscuroFilters where
+
+  mempty = noChiaroscuroFilters
+
+--------------------------------------------------
+
+-- | @'(<>)'@ is pointwise.
+
+instance Semigroup ChiaroscuroFilters where
+
+  ChiaroscuroFilters{ chiaroscuroBlacklist = lb, chiaroscuroWhitelist = lw } <> ChiaroscuroFilters{ chiaroscuroBlacklist = rb, chiaroscuroWhitelist = rw } =
+
+    ChiaroscuroFilters{chiaroscuroBlacklist,chiaroscuroWhitelist}
+
+    where
+
+    chiaroscuroBlacklist = lb <> rb
+    chiaroscuroWhitelist = lw <> rw
+
+--------------------------------------------------
+
+{-|
+
+@[ blacklist "A", whitelist "X", blacklist "B", whitelist "Y" ]
+@
+
+@ChiaroscuroFilters [ "A", "B" ] [ "X", "Y" ]
+@
+
+@[] :: ChiaroscuroFilters@ means "nothing is filtered".
 
 -}
 
-type FilePathFilters = These Blacklist Whitelist
+instance IsList ChiaroscuroFilters where
+
+  type Item ChiaroscuroFilters = ChiaroscuroFilter
+
+  fromList = fmap go > Fold.fold
+
+    where
+    go = either blacklistFilter whitelistFilter
+
+  toList (ChiaroscuroFilters{chiaroscuroBlacklist,chiaroscuroWhitelist}) =
+
+    fromBlacklist chiaroscuroBlacklist <> fromWhitelist chiaroscuroWhitelist
+
+    where
+    fromBlacklist (Blacklist bs) = bs & (Set.toList > fmap blacklist)
+
+    fromWhitelist = \case
+      Whitelist Nothing   -> []
+      Whitelist (Just ws) -> ws & (Set.toList > fmap whitelist)
+
+--------------------------------------------------
+
+{-| Everything is permitted and nothing is restricted.
+
+@≡ 'ChiaroscuroFilters' 'emptyBlacklist' 'fullWhitelist'@
+
+-}
+
+noChiaroscuroFilters :: ChiaroscuroFilters
+noChiaroscuroFilters = ChiaroscuroFilters emptyBlacklist fullWhitelist
+
+--------------------------------------------------
+
+blacklistFilter :: GlobPattern -> ChiaroscuroFilters
+blacklistFilter = Set.singleton > go
+  where
+
+  go x = ChiaroscuroFilters (Blacklist x) mempty
+
+--------------------------------------------------
+
+whitelistFilter :: GlobPattern -> ChiaroscuroFilters
+whitelistFilter = Set.singleton >
+  go
+  where
+
+  go x = ChiaroscuroFilters mempty (Whitelist (Just x))
 
 --------------------------------------------------
 --------------------------------------------------
 
-{-| 
+{-| a 'ChiaroscuroFilter' is a filter (a 'GlobPattern'),
+either blacklisted or whitelisted.
+
+-}
+
+type ChiaroscuroFilter = (Either GlobPattern GlobPattern)
+
+--------------------------------------------------
+
+{-| a blacklisted pattern.
+
+= Implementation
+
+@≡ 'Left'@
+
+-}
+
+blacklist :: GlobPattern -> ChiaroscuroFilter
+blacklist = Left
+
+--------------------------------------------------
+
+{-| a whitelisted pattern.
+
+= Implementation
+
+@≡ 'Right'@
+
+-}
+
+whitelist :: GlobPattern -> ChiaroscuroFilter
+whitelist = Right
+
+--------------------------------------------------
+--------------------------------------------------
+
+{-|
+
+Merging two whitelists takes their intersection. That is,
+something is permitted only if it's permitted by both whitelists.
 
 -}
 
 newtype Whitelist = Whitelist
 
-  (Set FilePathRegex)
+  (Maybe (Set GlobPattern))
 
   deriving stock    (Generic)
   deriving stock    (Show, Read)
   deriving newtype  (Eq, Ord)
 
-  deriving newtype  (Semigroup, Monoid)
+  --deriving newtype  (Semigroup, Monoid)
   deriving newtype  (NFData{-, Hashable-})
 
 --------------------------------------------------
 
+-- | @≡ 'fullWhitelist'@
+
+instance Monoid Whitelist where
+
+  mempty = fullWhitelist
+
+--------------------------------------------------
+
+-- | @('<>') ≡ 'Set.intersection'@
+
+instance Semigroup Whitelist where
+
+  Whitelist Nothing <> ws = ws
+  ws <> Whitelist Nothing = ws
+
+  Whitelist (Just ws) <> Whitelist (Just vs) =
+
+    Whitelist (Just (ws `Set.intersection` vs))
+
+--------------------------------------------------
+
+{-
+
+@[] :: Whitelist@ means "nothing is permitted"
+(N.B. it's not 'fullWhitelist').
+
+-}
+
 instance IsList Whitelist where
 
-  type Item (Whitelist) = FilePathRegex
+  type Item (Whitelist) = GlobPattern
 
-  fromList = Set.fromList > coerce
-  toList   = coerce > Set.toList
+  fromList = Set.fromList > Just > coerce
+  toList   = coerce > maybe Set.empty id > Set.toList -- TODO «toList» is ill-defined; «fullWhitelist» "should" be all (infinite) possible strings.
+
+--------------------------------------------------
+
+-- | a 'Whitelist' representing everything being permitted.
+
+fullWhitelist :: Whitelist
+fullWhitelist = Whitelist Nothing
 
 --------------------------------------------------
 --------------------------------------------------
 
 {-| 
 
+Merging two blacklists takes their union. That is,
+something is restricted if it's in either blacklist.
+
 -}
 
 newtype Blacklist = Blacklist
 
-  (Set FilePathRegex)
+  (Set GlobPattern)
 
   deriving stock    (Generic)
   deriving stock    (Show, Read)
   deriving newtype  (Eq, Ord)
 
-  deriving newtype  (Semigroup, Monoid)
+  --deriving newtype  (Semigroup, Monoid)
   deriving newtype  (NFData{-, Hashable-})
 
 --------------------------------------------------
 
+-- | @≡ 'emptyBlacklist'@
+
+instance Monoid Blacklist where
+
+  mempty = emptyBlacklist
+
+--------------------------------------------------
+
+-- | @('<>') ≡ 'Set.union'@
+
+instance Semigroup Blacklist where
+
+  (Blacklist ws) <> (Blacklist vs) = Blacklist (ws `Set.union` vs)
+
+--------------------------------------------------
+
+{-|
+@([] :: Blacklist) == 'emptyBlacklist'@
+-}
+
 instance IsList Blacklist where
 
-  type Item (Blacklist) = FilePathRegex
+  type Item (Blacklist) = GlobPattern
 
   fromList = Set.fromList > coerce
   toList   = coerce > Set.toList
+
+--------------------------------------------------
+
+-- | a 'Blacklist' representing nothing being restricted.
+
+emptyBlacklist :: Blacklist
+emptyBlacklist = Blacklist Set.empty
 
 --------------------------------------------------
 --------------------------------------------------
